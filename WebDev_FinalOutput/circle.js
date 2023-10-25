ad1ed68fcb800c3bffca9da15a8c8da98ce74705
unknown <Admin@CSLAB204.CSSERVER.local>
2023-10-25 19:15:00 +0800

    test

--- WebDev_FinalOutput/circle.js
+++ WebDev_FinalOutput/circle.js
@@ -1,17 +1,18 @@
 
 document.getElementById('s').addEventListener('click', function () {
     let r = parseInt(document.getElementById('radius').value);
+    let d = parseInt(document.getElementsByClassName('diameter').value);
     let operation = document.getElementById('s').value;
     
-    var circum;
-    var area;
+    var radius;
+    var diameter;
     switch (operation) {
         case 'solve':
-            area = Math.PI * (r * r);
-            circum = 2 * Math.PI * r;
+            diameter = 3.14 * r;
+            radius = 2 * 3.14 * r;
             break;
     }
-    document.getElementById('value').innerHTML = `<h3 style="font-family: 'lemonmilk', sans-serif;">Area: ${area} <br><br> Cicumference: ${circum}</h3>`;
+    document.getElementById('value').innerHTML = `<h3 style="font-family: 'lemonmilk', sans-serif;">Circumference (diameter): ${diameter} <br><br> Cicumference (radius): ${radius}</h3>`;
 
     document.getElementById('modal').style.display = 'block';
 })
